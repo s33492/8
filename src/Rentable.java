1a2a8a5b19e72981428646ace3b5564bfd5e3e91
s33492 <s33492@pjwstk.edu.pl>
2025-05-29 19:38:59 +0200

    Update Rentable.java

--- src/Rentable.java
+++ src/Rentable.java
@@ -1,10 +1,8 @@
 public interface Rentable {
     int MAX_RENTAL_DAYS = 14;
-
     void rent();
     void returnItem();
     boolean isAvailable();
-
     static double calculateLateFee(int daysLate) {
         return daysLate * 0.50;
     }
